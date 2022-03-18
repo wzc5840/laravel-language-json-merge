4a1310684ab9d8d269d692cdaf118ba6d2737a89
Symond Wang <wzc5840@gmail.com>
2022-03-18 10:17:02 +0900

    Backup

--- merge_language.py
+++ merge_language.py
@@ -1,8 +1,23 @@
 import sys
 import argparse
+import json
 
 
-def run(json_path_from, json_path_to, out_put_path):
+def do_merge(json_path_from, json_path_to, out_put_path, language):
+    output_dic = {}
+    with open(json_path_from, 'r') as load_from:
+        load_from_dict = json.load(load_from)
+        with open(json_path_to, 'r') as load_to:
+            load_to_dict = json.load(load_to)
+            # for key_from in load_from_dict:
+            #     print('key_from = ' + key_from)
+            #     print('key_from = ' + load_from_dict[key_from])
+            for key_to in load_to_dict:
+                print('key_from = ' + key_to)
+                print('key_from = ' + load_to_dict[key_to])
+
+
+def run(json_path_from, json_path_to, out_put_path, language):
     '''
     コマンド実行
     '''
@@ -13,6 +28,8 @@ def run(json_path_from, json_path_to, out_put_path):
         print('json_path_from = ' + json_path_from)
         print('json_path_to = ' + json_path_to)
         print('out_put_path = ' + out_put_path)
+        print('language = ' + language)
+        do_merge(json_path_from, json_path_to, out_put_path, language)
     except Exception as e:
         print('merge language json cause expection!!')
         print(e)
@@ -31,12 +48,15 @@ def main():
     parser.add_argument(
         '--json_path_to', help='Merge to', required=True)
     parser.add_argument(
-        '--out_put_path', help='Merge to', required=True)
+        '--out_put_path', help='Output to', required=True)
+    parser.add_argument(
+        '--language', help='Language', required=True)
     args = parser.parse_args()
     print('Command quit!')
     run(json_path_from=args.json_path_from,
         json_path_to=args.json_path_to,
-        out_put_path=args.out_put_path)
+        out_put_path=args.out_put_path,
+        out_put_path=args.language)
 
 
 if __name__ == '__main__':
